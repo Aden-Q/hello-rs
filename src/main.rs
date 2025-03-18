cfbfdecf2a8d00a86e754a076cd234030cbe411e
Aden-Q <qzc0512@gmail.com>
2025-03-18 14:53:59 -0500

    feat: concurrency with thread

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use std::fmt::format;
-use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_connection(stream),
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_connection(stream);
+                });
+            }
             Err(err) => eprintln!("Connection failed, {err}"),
         }
 
@@ -20,10 +24,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let req_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if req_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "assets/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "assets/404.html")
+    let (status_line, filename) = match &req_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "assets/index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "assets/index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "assets/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
