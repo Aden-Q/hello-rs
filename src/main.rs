57bece3e844f95541687599b1aadf2e8947354d1
Aden-Q <qzc0512@gmail.com>
2025-03-18 14:39:29 -0500

    feat: add 404 page

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let _http_req: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let req_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-    let contents = fs::read_to_string("assets/index.html").unwrap();
+    let (status_line, filename) = if req_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "assets/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "assets/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let resp = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n\n{contents}");
